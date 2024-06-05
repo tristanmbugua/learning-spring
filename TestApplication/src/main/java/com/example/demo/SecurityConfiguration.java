414ddf529592d9847957b8c10b1e1caec0483c6c
Tristan Mbugua <128255762+tristanmbugua@users.noreply.github.com>
2024-06-05 19:50:25 -0400

    Spring Security will be covered later. Spring Configuration for will be studied now instead.
    Spring Security is it's own highly-complex sub-system. Spring Security will be covered later. I will study Spring Configuration and environment manipulation now instead.

---
+++ TestApplication/src/main/java/com/example/demo/SecurityConfiguration.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.*;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfiguration {
+
+}
